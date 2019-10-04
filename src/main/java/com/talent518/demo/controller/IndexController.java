b20ab98aa4ccf735aa85fc8016761463daeb1184
ZhangBaoCai <talent518@yeah.net>
2019-10-04 22:18:40 +0800

    jsp view and jstl taglib.

--- src/main/java/com/talent518/demo/controller/IndexController.java
+++ src/main/java/com/talent518/demo/controller/IndexController.java
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
@@ -19,24 +19,11 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 
 @Controller
 public class IndexController {
-	@ResponseBody
 	@RequestMapping("/")
-	public String index(HttpServletRequest request) {
-		StringBuffer sb = new StringBuffer();
-		sb.append("<!DOCTYPE html>\n" + 
-				"<html>\n" + 
-				"<head>\n" + 
-				"	<title>spring-boot-web</title>\n" + 
-				"</head>\n" + 
-				"<body style=\"line-height:26px;font-size:16px;\">\n");
-		for(String url: urls()) {
-			if(url.indexOf('{') > -1)
-				sb.append(url).append("<br/>\n");
-			else
-				sb.append(String.format("<a href=\"%s\">%s</a><br/>\n", request.getContextPath() + url, url));
-		}
-		sb.append("</body></html>");
-		return sb.toString();
+	public String index(HttpServletRequest request, Model model) {
+		model.addAttribute("urls", urls());
+		model.addAttribute("baseUrl", request.getContextPath());
+		return "index";
 	}
 
 	@Autowired
